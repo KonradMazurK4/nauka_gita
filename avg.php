577cd89ebaf6af376641a4a3b09db125df8f5b84
KonradMazurK4 <wrx88748@student.wroclaw.merito.pl>
2024-03-10 10:46:22 +0100

    srednia wazona

--- avg.php
+++ avg.php
@@ -3,3 +3,15 @@ $oceny = array(3, 5, 2, 1, 4, 4);
 $srednia = array_sum($oceny)/count($oceny);
 echo round($srednia,1);
 ?>
+
+<?php
+$oceny = array(1,2,3,4,5);
+$wagi = array(5,4,3,2,1);
+$sumaWazona = 0;
+foreach( $oceny as $klucz => $ocena ){
+  $sumaWazona +=  $ocena * $wagi[$klucz];
+  $sumaWag += $wagi[$klucz];
+}
+ 
+$wynik = $sumaWazona / $sumaWag;
+?>
